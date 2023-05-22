26a3c01ec6774bc6ac7c3a8bcfc6162afd48f0ae
Chris Eberly <ceberly@users.noreply.github.com>
2023-05-22 12:38:09 -0400

    fix schema and basic generate command

---
+++ src/generate.js
@@ -0,0 +1,12 @@
+const generator = require('json-schema-to-typescript');
+
+(function() {
+  // XXX or env ?
+  const CURRENT_SCHEMA = 7;
+
+  console.log("generating schema...");
+  generator.compileFromFile("schema/7/vault.json")
+  .then(function(s) {
+    console.log(s);
+  });
+})();
