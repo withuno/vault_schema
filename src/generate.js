eb118ffdbbf532a5e4a078b12e5e688626f2bff8
Chris Eberly <ceberly@users.noreply.github.com>
2023-05-23 12:42:45 -0400

    write out the type file and git ignore it for now

--- src/generate.js
+++ src/generate.js
@@ -1,6 +1,8 @@
 //XXX: this is not ready yet. Chris
 const generator = require('json-schema-to-typescript');
 
+const fs = require('fs');
+
 (function() {
   // XXX or env ?
   const CURRENT_SCHEMA = 7;
@@ -8,6 +10,6 @@ const generator = require('json-schema-to-typescript');
   console.log("generating schema...");
   generator.compileFromFile("schemas/7/vault.json")
   .then(function(s) {
-    console.log(s);
+    fs.writeFileSync('vault.d.ts', s);
   });
 })();
