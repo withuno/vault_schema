0617e2e8b14809c5672e620577ed4c25175237b2
Colin Tremblay <colin@uno.app>
2023-05-24 17:32:58 -0600

    static

--- Sources/SchemaLoader.swift
+++ Sources/SchemaLoader.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public final class SchemaLoader {
-	public func loadSchema(version: Int) -> [String: Any]? {
+	public static func loadSchema(version: Int) -> [String: Any]? {
         guard let path = Bundle(for: Self.self).path(forResource: "vault", ofType: "json", inDirectory: "\(version)") else { return nil }
 		do {
             let data = try Data(contentsOf: URL(fileURLWithPath: path))
