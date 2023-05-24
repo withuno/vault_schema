710b93f8cd100344dc1b4fbb50c9951e3d2c21c9
Colin Tremblay <colin@uno.app>
2023-05-24 15:09:34 -0600

    add package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.4
+import PackageDescription
+
+let package = Package(
+    name: "VaultSchema",
+    platforms: [
+        .iOS(.v15), .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "VaultSchema",
+            targets: ["VaultSchema"]),
+    ],
+    targets: [
+        .target(
+            name: "VaultSchema",
+            path: ".",
+            exclude: ["src/*"],
+            resources: [
+                .process("schemas/*")
+            ]
+        )
+    ]
+)
