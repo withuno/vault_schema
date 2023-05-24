f3f610b8db5579452717305d425ac67907f9df3c
Colin Tremblay <colin@uno.app>
2023-05-24 17:27:34 -0600

    fix

--- Sources/Empty.swift
+++ Sources/Empty.swift
@@ -1,6 +1,8 @@
+import Foundation
+
 public final class SchemaLoader {
 	public func loadSchema(version: Int) -> [String: Any]? {
-		let path = Bundle(for: Self.self).path(forResource: "vault", ofType: "json", inDirectory: "\(version)")
+        guard let path = Bundle(for: Self.self).path(forResource: "vault", ofType: "json", inDirectory: "\(version)") else { return nil }
 		do {
             let data = try Data(contentsOf: URL(fileURLWithPath: path))
             return try JSONSerialization.jsonObject(with: data) as? [String: Any]
