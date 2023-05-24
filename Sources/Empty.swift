75ada5a2a4929baad61e259d67b2e4778661f5ad
Colin Tremblay <colin@uno.app>
2023-05-24 17:25:32 -0600

    pass the json from the schema loader

--- Sources/Empty.swift
+++ Sources/Empty.swift
@@ -1,1 +1,12 @@
-public final class Empty {}
+public final class SchemaLoader {
+	public func loadSchema(version: Int) -> [String: Any]? {
+		let path = Bundle(for: Self.self).path(forResource: "vault", ofType: "json", inDirectory: "\(version)")
+		do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            return try JSONSerialization.jsonObject(with: data) as? [String: Any]
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+	}	
+}
